02443171498739f15518ae97f30336483a733d27
LovelySehotra12 <l.sehotra@cybtekk.com>
2024-07-04 16:28:38 +0530

    change vercel file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.json({ message: "Express on Vercel" }));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () =>
+  console.log("Server ready on port http://localhost:3000.")
+);
 
 module.exports = app;
